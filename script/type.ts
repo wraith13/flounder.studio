722d35c317672d3bca45e1adf058db735103400b
道化師 <wraith13@hotmail.co.jp>
2024-02-15 00:05:46 +0900

    🛠️ 実装 ( WIP )

--- script/type.ts
+++ script/type.ts
@@ -2,11 +2,13 @@ import { Tektite } from "tektite.js/script/tektite-index";
 import { minamo } from "minamo.js";
 import localeEn from "@resource/lang.en.json";
 import localeJa from "@resource/lang.ja.json";
+import { FlounderStudio } from "@script/index";
 export module Type
 {
     export interface PageParams extends minamo.core.JsonableObject
     {
         // hash?: string;
     }
-    export type TektiteParams = Tektite.ParamTypes<PageParams, Resource.KeyType, typeof localeEn | typeof localeJa, typeof Clockworks.localeMaster>;
+    // export type TektiteParams = Tektite.ParamTypes<PageParams, Resource.KeyType, typeof localeEn | typeof localeJa, typeof FlounderStudio.localeMaster>;
+    export type TektiteParams = Tektite.ParamTypes<PageParams, string, typeof localeEn | typeof localeJa, typeof FlounderStudio.localeMaster>;
 }
